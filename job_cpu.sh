63619b8330f08076e1b0097307071b5eb10b5b7c
madsemildahlgaard <madsemildh@gmail.com>
2021-04-27 12:09:00 +0200

    Add job_cpu.sh

---
+++ job_cpu.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#BSUB -q hpc
+#BSUB -J dlg
+#BSUB -n 1
+#BSUB -W 23:00
+#BSUB -R "rusage[mem=32GB]"
+#BSUB -o logs/dlg_%J.out
+#BSUB -e logs/dlg_%J.err
+
+module load cuda/11.1
+source .venv/bin/activate
+
+which python3
+
+echo "Running script..."
+python3 run_experiment.py
